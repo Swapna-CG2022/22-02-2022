7b41999a7b51f612725fa2055e78dc03770c31e1
swapna <swapna-mori@gmail.com>
2022-02-21 16:21:19 +0530

    modified  code in Hello.java

--- FirstJavaProject/src/Hello.java
+++ FirstJavaProject/src/Hello.java
@@ -3,7 +3,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Dear Friends!!!!!");
-
+		System.out.println("Hello Dear Friends!!!!!");
+		System.out.println("Hello Dear Friends!!!!!");
+		System.out.println("Hello Dear Friends!!!!!");
+		System.out.println("Hello Dear Friends!!!!!");
+		
 	}
 
 }
